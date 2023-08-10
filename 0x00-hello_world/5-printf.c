e1797372c82699e8565d3c1f81fe19d629d7175e
IniGlowreeyah <1gloryini@gmail.com>
2023-08-10 10:06:06 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - printing output with printf
  *
- * Return: always(0)
+ * Return: always (0)
  *
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
