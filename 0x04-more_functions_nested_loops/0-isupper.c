cac0e0ba40c31805aaf873e6dc5889258172146f
IniGlowreeyah <1gloryini@gmail.com>
2023-08-17 02:42:23 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - detect upper case alpabets
+ * @c: input character
+ * Return: (1) for true (0) for false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{ return (0);
+	}
+}
