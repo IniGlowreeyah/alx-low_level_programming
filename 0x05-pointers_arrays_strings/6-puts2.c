33429f33edd913018d768d95535e667773e7a0d6
IniGlowreeyah <1gloryini@gmail.com>
2023-08-21 13:41:55 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,18 @@
 #include "main.h"
 /**
- *i puts2 - function should print only one character out of two
- * starting with the first one
+ * puts2 - this will print all even of a string
  * @str: input
  * Return: print
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
+		if (i % 2 == 0)
 		{
-			putchar(str[0]);
+			putchar(str[i]);
 		}
 	}
 	putchar('\n');
